51b74ffc8db26cd7e31bb4f66465c3bf224b14fd
Meman123 <87839716+Meman123@users.noreply.github.com>
2022-03-28 12:03:41 -0500

    c++

---
+++ Source/AdvancedFrameworkVR/Private/ActorTEST.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ActorTEST.h"
+
+// Sets default values
+AActorTEST::AActorTEST()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CustomRootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("CustomRoot"));
+	RootComponent = CustomRootComponent;
+
+	GrabableStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("GrabMesh"));
+	//GrabableStaticMesh->SetupAttachment(CustomRootComponent);
+	
+	
+
+}
+
+// Called when the game starts or when spawned
+void AActorTEST::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AActorTEST::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
